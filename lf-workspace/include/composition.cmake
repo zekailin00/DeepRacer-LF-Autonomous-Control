108f1af2bb5b2a908289b633a36ec5537bc7558c
zekailin00 <zekailin00@gmail.com>
2022-07-15 17:12:49 -0700

    uploaded dev manual

--- lf-workspace/include/composition.cmake
+++ lf-workspace/include/composition.cmake
@@ -1,3 +1,13 @@
 find_package(ament_cmake REQUIRED)
+
 find_package(camera_pkg REQUIRED)
 ament_target_dependencies(${LF_MAIN_TARGET} camera_pkg)
+
+find_package(deepracer_interfaces_pkg REQUIRED)
+ament_target_dependencies(${LF_MAIN_TARGET} deepracer_interfaces_pkg)
+
+find_package(servo_pkg REQUIRED)
+ament_target_dependencies(${LF_MAIN_TARGET} servo_pkg)
+
+find_package(deepracer_joy_control REQUIRED)
+ament_target_dependencies(${LF_MAIN_TARGET} deepracer_joy_control)
